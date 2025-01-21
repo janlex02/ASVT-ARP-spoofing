6242f566720649b0bbf5e2615123a7dcc5013ebe
janlex02 <>
2025-01-22 02:09:46 +0300

    `Added functionality to get current MAC address, validate new MAC address, and check if MAC address has been changed successfully.`

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,6 @@
 import subprocess
 import optparse
+import re
 
 def get_argumets():
     parser = optparse.OptionParser()
@@ -12,13 +13,50 @@ def get_argumets():
         parser.error("Please specify a new mac, use --help for more information")
     return options
 
+def get_current_mac(interface):
+    ifconfig_results = subprocess.check_output(["ifconfig", interface]).decode()
+    current__mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_results)
+    if current__mac:
+        print("[+]  Your current MAC is: " + str(current__mac.group(0)))
+        return current__mac.group(0)
+    else:
+        print(f"[-]  Could not find MAC address of {interface} interface")
+        return None
+
+def is_valid_mac(mac):
+    if re.match(r"^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$", mac):
+        return True
+    else:
+        print("[!]  You have entered an invalid MAC address")
+        return False
+
 def change_mac(interface, new_mac):
-    print("Changing MAC address for " + interface + " to " + new_mac)
+    print("[+]  Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-options = get_argumets()
-# change_mac(options.interface, options.new_mac)
-ifconfig_results = subprocess.check_output(["ifconfig"], options.interface)
-print(ifconfig_results)
+
+def main():
+    options = get_argumets()
+    if not is_valid_mac(options.new_mac):
+        return
+    current_mac = get_current_mac(options.interface)
+    if current_mac == options.new_mac:
+        print("[!]  Your MAC address is already: " + str(options.new_mac))
+        return
+    if current_mac == None:
+        return
+    change_mac(options.interface, options.new_mac)
+
+    new_mac = get_current_mac(options.interface)
+    if new_mac == options.new_mac:
+        print(f"[+]  MAC has been successfully changed to {new_mac}")
+    else:
+        print(f"[-]  Failed to change MAC address. Current MAC is still {new_mac}")
+# main()
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("[-]  Something went wrong")
